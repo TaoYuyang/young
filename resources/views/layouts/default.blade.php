fff7b6d2b207ef0803a59065b1866dc91e072563
young <young@qq.com>
2018-10-12 07:03:03 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>@yield('title', 'YoungWebApp') -- For Young 进阶学习Laravel</title>
-  </head>
-  <body>
+<head>
+  <title>@yield('title', 'YoungWebApp') -- For Young 进阶学习Laravel</title>
+  <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+@include('layouts._header')
+
+<div class="container">
+  <div class="col-md-offset-1 col-md-10">
     @yield('content')
-  </body>
+    @include('layouts._footer')
+  </div>
+</div>
+</body>
 </html>
